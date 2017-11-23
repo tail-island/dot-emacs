546e6c4a3008c9bbfb0ec02aaa7eb45c64a5fe9a
OJIMA Ryoji <ryo@ryo-MacBook-Pro.local>
2017-11-24 08:25:29 +0900

    auto generated code is deleted.

--- init.el
+++ init.el
@@ -203,17 +203,3 @@
 (init-c++-mode)
 (init-elpy)
 (init-markdown-mode)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (markdown-mode helm exec-path-from-shell elpy cider))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background "#300a24" :foreground "white")))))
